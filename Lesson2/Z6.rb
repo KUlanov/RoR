72fe7421c29b2827854a3dd8c3513c025a29cd96
Konstantin Ulanov <GKondor@mail.ru>
2023-03-18 10:37:41 +0500

    completed Z6

---
+++ Lesson2/Z6.rb
@@ -0,0 +1,20 @@
+basket = {}
+loop do
+    puts "ВВедите <стоп> или <stop> в наименовании товара, если хотите выйти:"    
+    print "ВВедите наименование товара:"
+    d=gets.chomp
+    break if d == "стоп" or d == "stop"
+    print "ВВедите цену за еденицу товара:"
+    price=gets.chomp.to_f
+    print "ВВедите количество това:"
+    coint=gets.chomp.to_f
+    summ_i=price*coint
+    puts "Цена за #{d} , в количестве #{coint}= #{price*coint} "
+    basket[d] = [price, coint]
+end
+summ = 0
+basket.each {
+     |i, value|
+     summ += value[0] * value[1]
+   }
+puts summ
