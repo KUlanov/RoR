ea14dea4fb1e63df818284782cb9c6a641d4cdde
Konstantin Ulanov <GKondor@mail.ru>
2023-03-23 21:49:01 +0500

    Izmenil reshenie OOP

--- RZD/main.rb
+++ RZD/main.rb
@@ -1,122 +1,128 @@
 class Station
-  @@station_list
+  attr_accessor :name, :train_list
+
+  #@@station_list =[]
   def initialize(name)
     @name = name
-    @@station_list << @name
+    @train_list = []
+   # @@station_list << self
   end
 
   def input_train(train) 
-    @train_list << train
+    self.train_list << train
   end
 
   def output_train(train)
-    @train_list.delete(train)
+    self.train_list.delete(train)
   end
 
   def show_train_list
-    @train_list
+    self.train_list.each {|train| puts train.number}
   end
 
   def show_train_tips
-
+    @train_list
   end
 end
 
 
 class Route
+  attr_accessor :route_list
+  
   def initialize(f_station, l_station)
-    @rout_list = [f_station, l_station]
+    @route_list = [f_station, l_station]
   end
 
   def add_route_station(station)
-    @rout_list.insert(-2, station)
+    self.route_list.insert(-2, station)
   end
 
   def del_route_station(station)
-    @rout_list.delete(station)
+    self.route_list.delete(station)
   end 
 
-  def show_rout_list
-    @rout_list
+  def show_route_list
+    self.route_list.each { |station| puts station.name }
   end
 end
 
 
 class Train
-  def initialize(number_train, type_train, namber_wagons) 
-    @number_train = number_train
-    @type_train = type_train
-    @namber_wagons = namber_wagons
+  attr_reader :number, :type
+  attr_accessor :speed, :route, :wagons
+  
+  def initialize(number_train, type_train, wagons) 
+    @number = number_train
+    @type = type_train
+    @wagons = wagons
     @speed = 0
   end
 
   def speed_train_up
-    @speed += 10
+    self.speed += 10
   end
 
   def speed_train
-    @speed
+    self.speed
   end
 
   def speed_train_down
-    if @speed > 10
-      @speed -=10  
+    if self.speed > 10
+      self.speed -=10  
     else 
-      @speed = 0
+      self.speed = 0
     end
   end
 
   def speed_train_stop
-    @speed = 0
+    self.speed = 0
   end
-
-  def namber_wagons
-    @namber_wagons
-  end
-
+  
   def add_wagons
-    if @speed == 0
-         @namber_wagons +=1 
+    if self.speed == 0
+         self.wagons +=1 
     else 
       puts "Поезд движеться. Нельзя прицепить вагоны!"
     end
   end
 
   def del_wagons
-    if @speed == 0
-         @namber_wagons -=1 
+    if self.speed == 0
+         self.wagons -=1 
     else 
       puts "Поезд движеться. Нельзя отцепить вагоны!"
     end
   end
 
   def add_route(route) #новый маршрут
-    @route = route
+    self.route = route
     @train_station = 0
+=begin  
     @@station_list.each do |i, value|
       if @route[i].show_train_list.include? (self)# проверяем был ли этот поезд уже на одной из станций
         @route[i].output_train(self) # если был удаляем его, т.к. задан новый маршрут
       end
     end
-    @route[0].input_train(self)
+=end    
+    self.route.route_list[0].input_train(self)
     
   end
 
-  def train_rout_up
-    unless @route.rout_list[@train_station+1] == nill
+  def train_route_up
+    unless self.route.route_list[@train_station+1] == nil
       @train_station +=1
-      @route[@train_station-1].output_train(self)
-      @route[@train_station].input_train(self)
+      self.route.route_list[@train_station-1].output_train(self)
+      self.route.route_list[@train_station].input_train(self)
     else
       puts "Вы на конечной станции маршрута!"
     end
   end
 
-  def train_rout_down
+  def train_route_down
     if @train_station > 0
       @train_station -=1
-      @route[@train_station+1].output_train(self)
-      @route[@train_station].input_train(self)
+      self.route.route_list[@train_station+1].output_train(self)
+      self.route.route_list[@train_station].input_train(self)
     else
       puts "Вы на первой станции маршрута!"
     end
